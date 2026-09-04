ceabd283d4206231914cb5a93267b8ceeb6ca6ee
naviumzug <info@naviumzug.com>
2026-09-04 13:08:42 +0200

    Integrate Google Tag Manager into layout
    Added Google Tag Manager script for tracking.

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,16 @@ export const metadata = {
     locale: 'de_DE',
     type: 'website'
   }
-};
+};<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18068033913">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18068033913');
+</script>
 
 export default function RootLayout({ children }) {
   return (
